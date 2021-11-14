47a157117bf82ebcb0f916781f2ee9d88e357d61
Ashley Terstriep <aterstriep@vervocity.io>
2021-11-14 14:34:24 -0600

    Added RecipeContainer component

---
+++ src/components/RecipeContainer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import useSaveRecipe from '../hooks/useSaveRecipe';
+import { Link } from "gatsby";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+import Container from "./Container";
+import Badge from "./Badge";
+
+export default function RecipeContainer({ recipe, savedRecipes, updateSavedRecipes }) {
+
+    const savedRecipe = useSaveRecipe(recipe.id, savedRecipes) ? "saved-recipe" : "";
+
+    return (
+        <div
+            className={`recipe-container ${savedRecipe}`}
+            recipe_id={recipe.id}
+            key={recipe.id}
+        >
+            <FontAwesomeIcon icon={faHeart} onClick={() => updateSavedRecipes(recipe)} />
+            <Link to={`/recipe?${recipe.id}`} state={{ activeRecipe: recipe.id }}>
+                <img src={recipe.image} />
+            </Link>
+            <Container className="mealtypes" padding="0px">
+                {recipe.dishTypes.map((type, index) => {
+                    return <Badge key={index}>{type}</Badge>
+                })}
+            </Container>
+            <Link to={`/recipe?${recipe.id}`} state={{ activeRecipe: recipe.id }}><h3>{recipe.title}</h3></Link>
+        </div>
+    )
+}
