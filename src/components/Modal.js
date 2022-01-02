d11378c64651c347d2d4e546944ee85b974e50db
Ashley Terstriep <aterstriep@vervocity.io>
2022-01-02 16:53:00 -0600

    Drag and drop styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,18 +6,21 @@ const Title = styled.h3`
     margin-top: 0;
 `;
 
-export default function Modal(props) {
+export default function Modal({id, className, children, title, setAdded}) {
 
     const handleClick = () => {
         document.getElementById("meal-plan-modal").style.display = "none";
+        if(setAdded) {
+            setAdded(false);
+        }
     }
     
     return (
-        <div id={props.id} className={`modal-wrapper ${props.className}`} >
+        <div id={id} className={`modal-wrapper ${className}`} >
             <div className="modal-overlay" onClick={() => handleClick()}></div>
             <Container className="modal-inner" padding="30px">
-                <Title className="modal-title">{props.title}</Title>
-                {props.children}
+                <Title className="modal-title">{title}</Title>
+                {children}
             </Container>
         </div>
     )
--- src/components/recipes/MealPlanModal.js
+++ src/components/recipes/MealPlanModal.js
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
 import Modal from "../Modal";
 
+import { Link } from "gatsby";
+
 export default function MealPlanModal({recipe, addRecipe, active}) {
 
     const [day, setDay] = useState("Monday");
+    const [added, setAdded] = useState(false);
 
     const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
@@ -21,20 +24,39 @@ export default function MealPlanModal({recipe, addRecipe, active}) {
         )
     }
 
+    const ModalContent = () => {
+        if(added == true) {
+            return (
+                <>
+                    <p><strong>{recipe.title}</strong> has been added to your meal plan.</p>
+                    <Link to="/meal-plan" className="button">View Meal Plan</Link>
+                </>
+            );
+        }
+        return (
+            <>
+                <p>Select a day of the week to add <strong>{recipe.title}</strong> to your meal plan:</p>
+                <form>
+                    <Days />
+                    <button onClick={(event) => handleClick(event)}>Add to Meal Plan</button>
+                </form>
+            </>
+        );
+    }
+
     const handleClick = (event) => {
         event.preventDefault();
         addRecipe(recipe, day);
         setDay("Monday");
-        document.getElementById("meal-plan-modal").style.display = "none";
+
+        setAdded(true);
+
+        // document.getElementById("meal-plan-modal").style.display = "none";
     }
 
     return (
-        <Modal className="meal-plan-modal" title="Add to meal plan" id="meal-plan-modal" >
-            <p>Select a day of the week to add <strong>{recipe.title}</strong> to your meal plan:</p>
-            <form>
-                <Days />
-                <button onClick={(event) => handleClick(event)}>Add to Meal Plan</button>
-            </form>
+        <Modal className="meal-plan-modal" title="Add to meal plan" id="meal-plan-modal" setAdded={setAdded} >
+            <ModalContent />
         </Modal>
     )
 
